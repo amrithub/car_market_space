51907d8c3c7c7d8b252767ff647d2bcc34bf8b73
amrit <amritsk1@yahoo.com>
2020-02-28 22:26:11 +1100

    home page edited

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :cars
-  # get "/", to: "pages#home", as: "root"
+  get "/", to: "pages#home", as: "root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
