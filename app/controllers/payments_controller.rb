3850b1d747fe9183a0a5184aa73dd32f45ba328d
amrit <amritsk1@yahoo.com>
2020-03-12 22:51:10 +1100

    ultrahook added

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,17 +1,17 @@
 class PaymentsController < ApplicationController
-  before_action :authenticate_user!
-  # skip_before_action :verify_authenticity_token, only: [:webhook]
+  # before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token, only: [:webhook]
   def success
   end
-#   def webhook
-#     payment_id= params[:data][:object][:payment_intent]
-#     payment = Stripe::PaymentIntent.retrieve(payment_id)
-#     listing_id = payment.metadata.listing_id
-#     user_id = payment.metadata.user_id
+  def webhook
+    payment_id= params[:data][:object][:payment_intent]
+    payment = Stripe::PaymentIntent.retrieve(payment_id)
+    listing_id = payment.metadata.listing_id
+    user_id = payment.metadata.user_id
 
-#     p "car id " + car_id
-#     p "user id " + user_id
+    p "car id " + car_id
+    p "user id " + user_id
 
-#     status 200
-# end
+    status 200
+end
 end
